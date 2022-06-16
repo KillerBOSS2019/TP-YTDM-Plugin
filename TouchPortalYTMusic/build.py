b6f1fcaa1a42d3cee359f687535cf542137054b3
IMISSED <Damienweifen@gmail.com>
2022-06-16 11:15:15 -0700

    added buildscript

---
+++ TouchPortalYTMusic/build.py
@@ -0,0 +1,19 @@
+from TouchPortalAPI.tppbuild import *
+
+PLUGIN_MAIN = "TPYTMD.py"
+
+PLUGIN_EXE_NAME = "TPYTMD.exe"
+
+PLUGIN_EXE_ICON = "icon.ico"
+
+PLUGIN_ENTRY = "entry.tp"
+
+PLUGIN_ENTRY_INDENT = -1
+
+PLUGIN_ROOT = "TouchPortalYTMusic"
+
+PLUGIN_ICON = "icon.png"
+
+PLUGIN_VERSION = "2.1.2"
+
+validateBuild()
